eb6b8c7f30699efa564678f0cbbf170e102fc31d
Vasiliy Rodin <vrodin@Vasiliys-MacBook-Pro.local>
2015-02-06 16:32:03 -0800

    Added for loops and if loops to print data.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -36,13 +36,17 @@ var work = {
     "jobs": [
         {
             "employer": "ReServe Interactive",
-            "years": "2013-2015",
-            "position": "Support Technician"
+            "dates": "2013-2015",
+            "position": "Support Technician",
+            "description": "Provide technical and network support to customers over the phone or by email.",
+            "location" : "2021 W. Las Positas Court Suite 127 Livermore, CA 94551"
         },
         {
             "employer": "Centric Software",
-            "years": "2015-Present",
-            "position": "Quality Assurance Engineer"
+            "dates": "2015-Present",
+            "position": "Quality Assurance Engineer",
+            "description": "Stress test and migrate databases to new instaltions of the software.",
+            "location" : "485 Alberto Way #200, Los Gatos, CA 95032"
         }
     ]
 };
@@ -52,7 +56,7 @@ var projects = {
             "name": "BootStrap Practice Website",
             "years": "2014-2015",
             "url": "www.vasiliyrodin.com",
-            "description": "This is a website that I use to practice web development on"
+            "description": "This is a website that I use to practice front end web development."
         },
         {
             "name": "Solar Calendar",
@@ -72,16 +76,16 @@ $("#header").prepend(formattedName);
 $("#header").append()
 
 
-if(bio.contact !== null){
-    var formattedMobile = HTMLmobile.replace("%data%",bio.contact.mobile);
+if (bio.contact !== null) {
+    var formattedMobile = HTMLmobile.replace("%data%", bio.contact.mobile);
     $("#topContacts").append(formattedMobile);
-    var formattedMobile = HTMLemail.replace("%data%",bio.contact.email);
+    var formattedMobile = HTMLemail.replace("%data%", bio.contact.email);
     $("#topContacts").append(formattedMobile);
-    var formattedMobile = HTMLtwitter.replace("%data%",bio.contact.twitter);
+    var formattedMobile = HTMLtwitter.replace("%data%", bio.contact.twitter);
     $("#topContacts").append(formattedMobile);
-    var formattedMobile = HTMLgithub.replace("%data%",bio.contact.github);
+    var formattedMobile = HTMLgithub.replace("%data%", bio.contact.github);
     $("#topContacts").append(formattedMobile);
-    
+
 }
 
 
@@ -96,13 +100,22 @@ if (bio.skills.length > 0) {
     formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
     $("#skills").append(formattedSkill);
 }
+function displayWork() {
+    for (job in work.jobs) {
+        $("#workExperience").append(HTMLworkStart);
 
-for(job in work.jobs) {
-    $("#workExperience").append(HTMLworkStart);
-    
-    var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
-    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].position);
-    var formattedEmployerTitle = formattedEmployer + formattedTitle;
-    
-    $(".work-entry:last").append(formattedEmployerTitle);
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].position);
+        var formattedEmployerTitle = formattedEmployer + formattedTitle;
+
+        $(".work-entry:last").append(formattedEmployerTitle);
+
+        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+        $(".work-entry:last").append(formattedDates);
+
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+        $(".work-entry:last").append(formattedDescription);
+    }
 }
+
+displayWork();
